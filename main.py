99be2b6c7130fd578036cc5ebdb64e991b1ecc0e
Krutika9021 <krutikasalve24@gmail.com>
2024-12-28 00:22:05 +0530

    added

---
+++ main.py
@@ -0,0 +1,10 @@
+#addition
+# Input two numbers
+a = int(input("Enter the first number (A): "))
+b = int(input("Enter the second number (B): "))
+
+# Calculate the sum
+result = a + b
+
+# Print the result
+print(f"The sum of {a} and {b} is: {result}")
